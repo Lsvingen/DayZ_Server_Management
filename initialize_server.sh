1424e1bf5926458286e42612f2f6d9fe6a1cebdf
Lars Svingen <lars.svingen@gmail.com>
2025-04-28 15:11:53 +0200

    Update initialize_server.sh

--- initialize_server.sh
+++ initialize_server.sh
@@ -4,7 +4,7 @@
 #############################################
 
 #Enable SSH, restrictions for management are done via NSG from the Terraform deployment
-sudo apt install openssh-server
+#sudo apt install openssh-server
 
 #Download and execute DayZ server management script
 #wget -qO - https://github.com/Lsvingen/DayZ_Server_Management/blob/main/dayzserver.sh | bash -s param1 param2 ...
